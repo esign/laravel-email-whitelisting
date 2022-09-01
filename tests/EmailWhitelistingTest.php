3a0c62f7d303f6ef4b6477e5b428db25e0a6e660
Seppe Clottemans <seppe@esign.eu>
2022-09-01 15:57:49 +0200

    Tests: cc and bcc adresses + config test

--- tests/EmailWhitelistingTest.php
+++ tests/EmailWhitelistingTest.php
@@ -5,28 +5,12 @@ namespace Esign\EmailWhitelisting\Tests;
 use Esign\EmailWhitelisting\Mail\TestMail;
 use Esign\EmailWhitelisting\Models\WhitelistedEmailAddress;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Mail\SentMessage;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Mail;
-use Symfony\Component\Mime\Address;
 
 class EmailWhitelistingTest extends TestCase
 {
     use RefreshDatabase;
-
-    public function getAddresses(SentMessage $mail, string $type = 'To'): array
-    {
-        $recipients = $mail->getSymfonySentMessage()->getOriginalMessage()->{'get' . $type}();
-
-        return $this->addressesToString($recipients);
-    }
-
-    protected function addressesToString(array $addresses): array
-    {
-        return collect($addresses)->map(function (Address $item) {
-            return $item->getAddress();
-        })->toArray();
-    }
 
     /** @test */
     public function it_can_whitelist_email_addresses()
@@ -39,6 +23,59 @@ class EmailWhitelistingTest extends TestCase
         $recipients = $this->getAddresses($mail);
 
         $this->assertEquals(['test@esign.eu'] , $recipients);
+    }
+
+    /** @test */
+    public function it_can_whitelist_email_addresses_in_cc()
+    {
+        Config::set('email-whitelisting.whitelist_mails', true);
+        Config::set('email-whitelisting.redirect_mails', false);
+        WhitelistedEmailAddress::create(['email' => 'test@esign.eu']);
+        WhitelistedEmailAddress::create(['email' => 'cc@esign.eu']);
+
+        $mail = Mail::to(['test@esign.eu'])->cc(['cc@esign.eu', 'cc2@esign.eu'])->send(new TestMail());
+        $ccRecipients = $this->getAddresses($mail, 'Cc');
+
+        $this->assertEquals(['cc@esign.eu'] , $ccRecipients);
+    }
+
+    /** @test */
+    public function it_can_whitelist_email_addresses_in_bcc()
+    {
+        Config::set('email-whitelisting.whitelist_mails', true);
+        Config::set('email-whitelisting.redirect_mails', false);
+        WhitelistedEmailAddress::create(['email' => 'test@esign.eu']);
+        WhitelistedEmailAddress::create(['email' => 'bcc@esign.eu']);
+
+        $mail = Mail::to(['test@esign.eu'])->bcc(['bcc@esign.eu', 'bcc2@esign.eu'])->send(new TestMail());
+        $bccRecipients = $this->getAddresses($mail, 'Bcc');
+
+        $this->assertEquals(['bcc@esign.eu'] , $bccRecipients);
+    }
+
+    /** @test */
+    public function it_can_disable_email_whitelisting()
+    {
+        Config::set('email-whitelisting.whitelist_mails', false);
+        Config::set('email-whitelisting.redirect_mails', false);
+        WhitelistedEmailAddress::create(['email' => 'test@esign.eu']);
+
+        $mail = Mail::to(['test@esign.eu', 'agf@esign.eu'])->send(new TestMail());
+        $recipients = $this->getAddresses($mail);
+
+        $this->assertEquals(['test@esign.eu', 'agf@esign.eu'] , $recipients);
+    }
+
+    /** @test */
+    public function it_wont_throw_an_error_when_no_valid_email_addresses_are_given()
+    {
+        Config::set('email-whitelisting.whitelist_mails', true);
+        Config::set('email-whitelisting.redirect_mails', false);
+        WhitelistedEmailAddress::create(['email' => 'test@esign.eu']);
+
+        $mail = Mail::to(['test@test.eu', 'test2@test.eu'])->send(new TestMail());
+
+        $this->assertNull($mail);
     }
 
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,9 @@
 namespace Esign\EmailWhitelisting\Tests;
 
 use Esign\EmailWhitelisting\EmailWhitelistingServiceProvider;
+use Illuminate\Mail\SentMessage;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Symfony\Component\Mime\Address;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -16,5 +18,24 @@ abstract class TestCase extends BaseTestCase
     {
         $migration = include __DIR__ . '/../database/migrations/create_whitelist_email_addresses_table.php.stub';
         $migration->up();
+    }
+
+    /**
+     * @param SentMessage $mail
+     * @param string $type To|Cc|Bcc
+     * @return array
+     */
+    public function getAddresses(SentMessage $mail, string $type = 'To'): array
+    {
+        $recipients = $mail->getSymfonySentMessage()->getOriginalMessage()->{'get' . $type}();
+
+        return $this->addressesToString($recipients);
+    }
+
+    protected function addressesToString(array $addresses): array
+    {
+        return collect($addresses)->map(function (Address $item) {
+            return $item->getAddress();
+        })->toArray();
     }
 } 
