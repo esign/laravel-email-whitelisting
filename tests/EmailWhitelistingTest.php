073f74179edfd24d69803850699435f95735dcd3
Seppe Clottemans <seppe@esign.eu>
2022-09-01 15:43:34 +0200

    Tests: refactor test to check to addresses

--- tests/EmailWhitelistingTest.php
+++ tests/EmailWhitelistingTest.php
@@ -5,6 +5,7 @@ namespace Esign\EmailWhitelisting\Tests;
 use Esign\EmailWhitelisting\Mail\TestMail;
 use Esign\EmailWhitelisting\Models\WhitelistedEmailAddress;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Mail\SentMessage;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Mail;
 use Symfony\Component\Mime\Address;
@@ -12,6 +13,20 @@ use Symfony\Component\Mime\Address;
 class EmailWhitelistingTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function getAddresses(SentMessage $mail, string $type = 'To'): array
+    {
+        $recipients = $mail->getSymfonySentMessage()->getOriginalMessage()->{'get' . $type}();
+
+        return $this->addressesToString($recipients);
+    }
+
+    protected function addressesToString(array $addresses): array
+    {
+        return collect($addresses)->map(function (Address $item) {
+            return $item->getAddress();
+        })->toArray();
+    }
 
     /** @test */
     public function it_can_whitelist_email_addresses()
@@ -21,16 +36,9 @@ class EmailWhitelistingTest extends TestCase
         WhitelistedEmailAddress::create(['email' => 'test@esign.eu']);
 
         $mail = Mail::to(['test@esign.eu', 'agf@esign.eu'])->send(new TestMail());
-        $recipients = $this->addressesToString($mail->getEnvelope()->getRecipients());
+        $recipients = $this->getAddresses($mail);
 
         $this->assertEquals(['test@esign.eu'] , $recipients);
-    }
-
-    protected function addressesToString(array $addresses): array
-    {
-        return collect($addresses)->map(function (Address $item) {
-            return $item->getAddress();
-        })->toArray();
     }
 
 
