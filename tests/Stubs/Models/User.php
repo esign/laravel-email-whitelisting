c1080ead5204bf399992ef36f76d8d5443787268
Seppe Clottemans <seppe@esign.eu>
2022-09-05 11:26:49 +0200

    Tests: add notification whitelisting tests

---
+++ tests/Stubs/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Esign\EmailWhitelisting\Tests\Stubs\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = ['name', 'email'];
+}
---
+++ tests/Stubs/Notifications/TestNotification.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Esign\EmailWhitelisting\Tests\Stubs\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class TestNotification extends Notification
+{
+    use Queueable;
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    public function toArray($notifiable): array
+    {
+        return [];
+    }
+}
