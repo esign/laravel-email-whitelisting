c27154ba4d29a0232db200ff69bb9ef98f580963
Seppe Clottemans <seppe@esign.eu>
2022-09-05 14:10:01 +0200

    Add: redirect notifications

--- src/Listeners/WhitelistNotificationEmailAddresses.php
+++ src/Listeners/WhitelistNotificationEmailAddresses.php
@@ -5,8 +5,11 @@ namespace Esign\EmailWhitelisting\Listeners;
 use Esign\EmailWhitelisting\Models\WhitelistedEmailAddress;
 use Illuminate\Mail\Events\MessageSending;
 use Illuminate\Notifications\Events\NotificationSending;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Queue\QueueManager;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Queue;
 use Symfony\Component\Mime\Address;
 
 class WhitelistNotificationEmailAddresses
@@ -17,7 +20,7 @@ class WhitelistNotificationEmailAddresses
         if ($this->shouldWhitelistMailAddresses($event->channel)) {
 
             if (config('email-whitelisting.redirect_mails')) {
-                $this->redirectNotification($event);
+                return $this->redirectNotification($event);
             } else {
                 return $this->whitelistMailAddresses($event);
             }
@@ -40,10 +43,23 @@ class WhitelistNotificationEmailAddresses
         return WhitelistedEmailAddress::where('email', $toEmail)->exists();
     }
 
-    protected function redirectNotification(NotificationSending $event)
+    protected function redirectNotification(NotificationSending $event): bool
     {
-        // $emailsSendTo = WhitelistedEmailAddress::where('redirect_email', true)->pluck('email');
+        $emailsSendTo = WhitelistedEmailAddress::where('redirect_email', true)->pluck('email');
 
+        if (empty($emailsSendTo)) {
+            return false;
+        }
 
+        if ($emailsSendTo->contains($event->notifiable->email)) {
+            return true;
+        }
+
+        $class = get_class($event->notifiable);
+        $availableNotifiers = $class::whereIn('email', $emailsSendTo)->get();
+
+        Notification::send($availableNotifiers, $event->notification);
+
+        return false;
     }
 }
